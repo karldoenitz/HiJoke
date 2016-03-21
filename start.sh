fe621a3e243ad2ee0a34203bb9a227b5907d0b7f
lizhihao <karlvorndoenitz@gmail.com>
2016-03-21 23:33:10 +0800

    add vector

--- start.sh
+++ start.sh
@@ -1,6 +1,35 @@
 #!/bin/sh
 
-if [ $1 = "compile" ]; then
+if [ -z "$1" ]; then
+    echo "---start compile views---"
+    cd Templates
+    cppcms_tmpl_cc *.tmpl -o views.cpp
+    cd ..
+    echo "---compile success---"
+    echo "---start compile---"
+    c++ main.cpp settings.cpp \
+    Utils/Utils.cpp \
+    Utils/HttpClient.cpp \
+    DatabaseManager/DatabaseOperator.cpp \
+    DatabaseManager/joke/bean/Joke.cpp \
+    DatabaseManager/joke/manager/JokeManager.cpp \
+    DatabaseManager/user/bean/User.cpp \
+    DatabaseManager/comment/bean/Comment.cpp \
+    DatabaseManager/comment/manager/CommentManager.cpp \
+    DatabaseManager/user/manager/UserManager.cpp \
+    Application/BaseHandler.cpp \
+    Application/login/LoginHandler.cpp \
+    Application/logout/LogoutHandler.cpp \
+    Application/register/RegisterHandler.cpp \
+    Application/joke/JokeHandler.cpp \
+    Application/comment/CommentHandler.cpp \
+    Application/staticserver/StaticServer.cpp \
+    Templates/views.cpp \
+    -o main.o -lcppcms -lcppdb -lbooster -lcurl -std=c++11
+    echo "---compile success---"
+    echo "---run server---"
+    ./main.o -c config.json
+elif [ $1 = "compile" ]; then
     echo "---start compile---"
     c++ main.cpp settings.cpp \
     Utils/Utils.cpp \
@@ -29,35 +58,6 @@ elif [ $1 = "views" ]; then
     cd ..
     echo "---compile success---"
 elif [ $1 = "run" ]; then
-    echo "---run server---"
-    ./main.o -c config.json
-else
-    echo "---start compile views---"
-    cd Templates
-    cppcms_tmpl_cc *.tmpl -o views.cpp
-    cd ..
-    echo "---compile success---"
-    echo "---start compile---"
-    c++ main.cpp settings.cpp \
-    Utils/Utils.cpp \
-    Utils/HttpClient.cpp \
-    DatabaseManager/DatabaseOperator.cpp \
-    DatabaseManager/joke/bean/Joke.cpp \
-    DatabaseManager/joke/manager/JokeManager.cpp \
-    DatabaseManager/user/bean/User.cpp \
-    DatabaseManager/comment/bean/Comment.cpp \
-    DatabaseManager/comment/manager/CommentManager.cpp \
-    DatabaseManager/user/manager/UserManager.cpp \
-    Application/BaseHandler.cpp \
-    Application/login/LoginHandler.cpp \
-    Application/logout/LogoutHandler.cpp \
-    Application/register/RegisterHandler.cpp \
-    Application/joke/JokeHandler.cpp \
-    Application/comment/CommentHandler.cpp \
-    Application/staticserver/StaticServer.cpp \
-    Templates/views.cpp \
-    -o main.o -lcppcms -lcppdb -lbooster -lcurl -std=c++11
-    echo "---compile success---"
     echo "---run server---"
     ./main.o -c config.json
 fi
